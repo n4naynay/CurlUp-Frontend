4bde44738d73ff264f03f5bd430ca31e99c4ce93
Blue-Lambda-University <justin.weber@bluelambdatechnologies.com>
2025-06-15 10:18:43 -0400

    code changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -30,7 +30,7 @@ export default function Login() {
     setSuccess("");
 
     try {
-      const response = await fetch("http://13.220.150.65/api/users/login", {
+      const response = await fetch("http://13.220.150.65:8000/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
